fe87a2c5c8d551026e25300e613cdc0353ff282b
Anurag Kapur <anurag@beancrunch.com>
2016-07-06 20:55:59 +0100

    Add alternate solution based on bottom up DP approach and update notes on runtime complexities

--- src/main/java/com/anuragkapur/hackerrank/algorithms/dp/FibonacciModified.java
+++ src/main/java/com/anuragkapur/hackerrank/algorithms/dp/FibonacciModified.java
@@ -8,10 +8,6 @@ import java.util.Scanner;
  *
  * Status: Correct
  *
- * Run time complexity:
- * To compute the nth modified fib number, we need to compute all previous fib numbers. Thus, O(n), where n is the term
- * whose value is required
- *
  * @author anuragkapur
  */
 public class FibonacciModified {
@@ -26,10 +22,20 @@ public class FibonacciModified {
         String b = splits[1];
         int n = Integer.parseInt(splits[2]);
 
-        System.out.println(computeNth(a, b, n));
+        //System.out.println(computeNth(a, b, n));
+        System.out.println(computeNth1(a, b, n));
     }
 
+    /*
+     * Simple recursive approach, with repeated sub-problems
+     *
+     * Run time complexity:
+     * T(n) = T(n-1) + T(n-2) + O(1)
+     *      = O(2^n)
+     *
+     */
     private static BigInteger computeNth(String a, String b, int n) {
+
 
         if (n == 1) {
             return new BigInteger(a);
@@ -39,5 +45,26 @@ public class FibonacciModified {
             BigInteger n1 = computeNth(a, b, n-1);
             return n1.pow(2).add(computeNth(a, b, n-2));
         }
+    }
+
+    /*
+     * Bottom up, DP
+     *
+     * Run time complexity:
+     * To compute the nth modified fib number, we need to compute all previous fib numbers.
+     * Thus, O(n), where n is the term whose value is required.
+     */
+    private static BigInteger computeNth1(String a, String b, int n) {
+
+        BigInteger[] series = new BigInteger[n];
+
+        series[0] = new BigInteger(a);
+        series[1] = new BigInteger(b);
+
+        for (int i=2; i<n; i++) {
+            series[i] = series[i-1].pow(2).add(series[i-2]);
+        }
+
+        return series[n-1];
     }
 }
